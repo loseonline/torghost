55b828e482a4a5f546f5bcb2c73ae598fa1eead5
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-09-10 23:34:27 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2021-2022 by TeamLose@Github, < https://github.com/usermusti >.
+#
+# This file is part of < https://github.com/usermusti/torhost > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/usermusti>
+#
+# All rights reserved.
+echo "Torghost installer v3.0"
+echo "Installing prerequisites "
+sudo apt-get install tor python3-pip -y 
+echo "Installing dependencies "
+sudo pip3 install -r requirements.txt 
+mkdir build
+cd build
+cython3 ../torghost.py --embed -o torghost.c --verbose
+if [ $? -eq 0 ]; then
+    echo [SUCCESS] Generated C code
+else
+    echo [ERROR] Build failed. Unable to generate C code using cython3
+    exit 1
+fi
+gcc -Os -I /usr/include/python3.8 -o torghost torghost.c -lpython3.8 -lpthread -lm -lutil -ldl
+if [ $? -eq 0 ]; then
+    echo [SUCCESS] Compiled to static binay 
+else
+    echo [ERROR] Build failed
+    exit 1
+fi
+sudo cp -r torghost /usr/bin/
+if [ $? -eq 0 ]; then
+    echo [SUCCESS] Copied binary to /usr/bin 
+else
+    echo [ERROR] Unable to copy
+    ecit 1
+fi
